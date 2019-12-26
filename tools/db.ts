d88271307776badc4f32c56e64f7ddf512ff599b
Mirco Kraenz <kraenzmirco@gmail.com>
2019-12-26 19:28:36 +0100

    FIX #1 format of db.json with levels

--- tools/db.ts
+++ tools/db.ts
@@ -3,4 +3,4 @@ import { levelRows } from "../src/components/levels.data";
 // export json by running the following command from project root
 // ts-node -O '{"module": "commonjs"}' ./tools/db.ts > db.json
 
-console.log(JSON.stringify(levelRows));
+console.log(JSON.stringify({ levels: levelRows }));
