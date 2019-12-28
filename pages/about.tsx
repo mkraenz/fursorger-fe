99cabaf16f3b0407ba72f63c460f521b2cc9a355
Mirco Kraenz <kraenzmirco@gmail.com>
2019-12-28 10:44:49 +0100

    UPDATE #1 edit about page to trigger zeit now deployment

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,35 +1,30 @@
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
-import MuiLink from "@material-ui/core/Link";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import React from "react";
+import Footer from "../src/components/Footer";
+import Header from "../src/components/Header";
+import HeroUnit from "../src/components/HeroUnit";
 import Link from "../src/Link";
 import ProTip from "../src/ProTip";
 
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {"Copyright © "}
-            <MuiLink color="inherit" href="https://material-ui.com/">
-                Your Website
-            </MuiLink>{" "}
-            {new Date().getFullYear()}
-            {"."}
-        </Typography>
-    );
-}
-
 export default function About() {
     return (
-        <Container maxWidth="sm">
-            <Box my={4}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    Created from Next.js with TypeScript example
-                </Typography>
-                <Link href="/">Go to the main page</Link>
-                <ProTip />
-                <Copyright />
-            </Box>
-        </Container>
+        <React.Fragment>
+            <CssBaseline />
+            <Header />
+            <HeroUnit />
+            <Container maxWidth="sm">
+                <Box my={4}>
+                    <Typography variant="h4" component="h1" gutterBottom>
+                        Created from Next.js with TypeScript example
+                    </Typography>
+                    <Link href="/">Go to the main page</Link>
+                    <ProTip />
+                </Box>
+            </Container>
+            <Footer />
+        </React.Fragment>
     );
 }
