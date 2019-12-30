130a80f682292ff68372144423c09ed42f3dbff0
Mirco Kraenz <kraenzmirco@gmail.com>
2019-12-30 09:34:47 +0100

    CONFIG #1 on localhost, work with local server

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,8 @@
 export const CONFIG = {
     levelsMetadataUri:
-        // "http://localhost:3140/prod/levels" ||
+        (process.browser &&
+            window.location.hostname === "localhost" &&
+            "http://localhost:3140/prod/levels") ||
         "https://r7qyjnm5u0.execute-api.eu-central-1.amazonaws.com/prod/prod/levels" ||
         "https://my-json-server.typicode.com/proSingularity/fursorger-fe/levels",
     emailJs: {
