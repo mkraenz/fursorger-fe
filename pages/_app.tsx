9eeea66f96c9cc905af7e26a1b22cbc9ee7f4c62
Mirco Kraenz <kraenzmirco@gmail.com>
2019-12-31 13:05:48 +0100

    REPLACE #1 inspectlet by hotjar
    Reason: For hotjar it is much easier to find proper privacy policy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import Head from "next/head";
 import React from "react";
 import ReactGA from "react-ga";
 import { Provider } from "react-redux";
+import { initHotjar } from "../src/api/hotjar";
 import { isProductionBrowser } from "../src/isProductionBrowser";
 import { makeStore } from "../src/redux/store";
 import theme from "../src/theme";
@@ -27,9 +28,9 @@ class MyApp extends App<{ store: ReturnType<typeof makeStore> }> {
             <React.Fragment>
                 <Head>
                     <title>Fursorger Game</title>
-                    {isProductionBrowser && (
-                        <script type="text/javascript" id="inspectletjs">
-                            {initInspectlet() as any}
+                    {process.browser && (
+                        <script type="text/javascript" id="hotjar">
+                            {initHotjar() as any}
                         </script>
                     )}
                 </Head>
@@ -46,33 +47,6 @@ class MyApp extends App<{ store: ReturnType<typeof makeStore> }> {
 }
 
 export default withRedux(makeStore as any)(MyApp);
-
-const initInspectlet = () => {
-    // @ts-ignore
-    const window_: any = window;
-    window_.__insp = window_.__insp || [];
-    // @ts-ignore
-    __insp.push(["wid", 2004692989]);
-    (function() {
-        function __ldinsp() {
-            var insp = document.createElement("script");
-            insp.type = "text/javascript";
-            insp.async = true;
-            insp.id = "inspsync";
-            insp.src =
-                ("https:" == document.location.protocol ? "https" : "http") +
-                "://cdn.inspectlet.com/inspectlet.js?wid=2004692989&r=" +
-                Math.floor(new Date().getTime() / 3600000);
-            var x: any = document.getElementsByTagName("script")[0];
-            x.parentNode.insertBefore(insp, x);
-        }
-        document.readyState != "complete"
-            ? window_.attachEvent
-                ? window_.attachEvent("onload", __ldinsp)
-                : window_.addEventListener("load", __ldinsp, false)
-            : __ldinsp();
-    })();
-};
 
 const initGoogleAnalytics = () => {
     if (isProductionBrowser) {
