2b3349f350ca47ee68c2714cf80a58fdf0a3a26c
Mirco Kraenz <kraenzmirco@gmail.com>
2020-04-05 15:28:05 +0900

    REFACTOR #1 replace many ts-ignore  by single @ts-nocheck
    possible since TS 3.7

--- src/api/hotjar.ts
+++ src/api/hotjar.ts
@@ -1,26 +1,18 @@
+// @ts-nocheck
 export const initHotjar = () => {
     console.log("initializing hotjar");
     (function(h, o, t, j, a, r) {
-        // @ts-ignore
         h.hj =
-        // @ts-ignore
-        h.hj ||
-        function() {
-            // @ts-ignore
-            (h.hj.q = h.hj.q || []).push(arguments);
-        };
-        // @ts-ignore
+            h.hj ||
+            function() {
+                (h.hj.q = h.hj.q || []).push(arguments);
+            };
         h._hjSettings = { hjid: 1627577, hjsv: 6 };
-        // @ts-ignore
         a = o.getElementsByTagName("head")[0];
-        // @ts-ignore
         r = o.createElement("script");
-        // @ts-ignore
         r.async = 1;
-        // @ts-ignore
         r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        // @ts-ignore
         a.appendChild(r);
-        console.log("finished initilizing hotjar");
+        console.log("finished initializing hotjar");
     })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
 };
