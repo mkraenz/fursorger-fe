c17a09e78031318235a990bfc49ee3705943bdd9
Mirco Kraenz <kraenzmirco@gmail.com>
2020-04-05 13:30:26 +0900

    CHANGE #1 like button only clickable once (until reload)

--- src/components/levels-table/LikeButton.tsx
+++ src/components/levels-table/LikeButton.tsx
@@ -1,18 +1,25 @@
 import IconButton from "@material-ui/core/IconButton";
 import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import { useState } from "react";
 
-const LikeButton = (props: { likes: number; onClick: () => void }) => (
-    <span
-        style={{
-            whiteSpace: "nowrap",
-            overflow: "hidden",
-        }}
-    >
-        <IconButton onClick={props.onClick}>
-            <ThumbUpAltOutlinedIcon />
-        </IconButton>
-        {props.likes}
-    </span>
-);
-
+const LikeButton = (props: { likes: number; onClick: () => void }) => {
+    const [clicked, setClicked] = useState(false);
+    const onClick = () => {
+        setClicked(true);
+        props.onClick;
+    };
+    return (
+        <span
+            style={{
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+            }}
+        >
+            <IconButton onClick={onClick} disabled={clicked}>
+                <ThumbUpAltOutlinedIcon />
+            </IconButton>
+            {props.likes}
+        </span>
+    );
+};
 export default LikeButton;
